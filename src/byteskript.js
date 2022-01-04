5230b82aa5eb8aa89a3d242f57d8493c165547f4
Moderocky <admin@moderocky.com>
2022-01-04 14:18:23 +0000

    Catch common features and relevance.

--- src/byteskript.js
+++ src/byteskript.js
@@ -6,47 +6,100 @@ Category: scripting
 Website: https://docs.byteskript.org
 */
 
-module.exports = function (hljs) {
-    const KEYWORDS = [
-        "function",
-        "type",
-        "template",
-        "on",
-        "final",
-        "local",
-        "set",
-        "add",
-        "delete",
-        "remove",
-        "run",
-        "if",
-        "else",
-        "try",
-        "catch",
-        "while",
-        "loop",
-        "throw",
-        "return",
-        "break",
-        "continue",
-        "stop",
-        "exit",
-        "assert",
-        "null",
-        "none",
-        "true",
-        "false",
-        "new",
-    ]
+module.exports = function(hljs) {
+  const KEYWORDS = [
+    "function",
+    "on",
+    "type",
+    "template",
+    "final",
+    "local",
+    "set",
+    "add",
+    "delete",
+    "remove",
+    "run",
+    "if",
+    "else",
+    "try",
+    "catch",
+    "while",
+    "loop",
+    "throw",
+    "return",
+    "break",
+    "continue",
+    "stop",
+    "exit",
+    "assert",
+    "new"
+  ];
 
-    return {
-        aliases: ["byteskript", "bsk"],
-        keywords: KEYWORDS,
-        contains: [
-            hljs.C_LINE_COMMENT_MODE,
-            hljs.C_BLOCK_COMMENT_MODE,
-            hljs.C_NUMBER_MODE,
-            hljs.QUOTE_STRING_MODE,
-        ],
-    };
+  const ENTRIES = {
+    className: 'built_in',
+    relevance: 2,
+    begin: '\\b(verify|trigger|syntax)(?=:)'
+  };
+
+  const EVENTS = {
+    className: 'built_in',
+    relevance: 2,
+    begin: '(?<=on ).+(?=:)'
+  };
+
+  const FUNCTIONS = {
+    className: 'built_in',
+    relevance: 1,
+    begin: '(?<=function )[\\w_]+ ?(?=[\\(:])'
+  };
+
+  const FUNCTION_USE = {
+    className: 'built_in',
+    relevance: 1,
+    begin: '(?<!function )[\\w_]+\\(',
+    end: '\\)'
+  };
+
+  const LITERALS = {
+    className: 'literal',
+    relevance: 1,
+    begin: '\\b(null|none|false|true)\\b'
+  };
+
+  const NUMBERS = {
+    className: 'number',
+    relevance: 0,
+    begin: '\\b-?[0-9]+(\\.[0-9]+)?[dDlLfF]?\\b'
+  };
+
+  const VARIABLES = {
+    className: 'variable',
+    relevance: 2,
+    begin: '\\{[@_!\\$]?\\w[\\w_]*\\}'
+  };
+
+  const REGEX = {
+    className: 'literal',
+    relevance: 0,
+    begin: '/[^*].*/'
+  };
+
+  return {
+    aliases: ["byteskript", "bsk"],
+    keywords: KEYWORDS,
+    contains: [
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.C_BLOCK_COMMENT_MODE,
+      hljs.C_NUMBER_MODE,
+      hljs.QUOTE_STRING_MODE,
+      FUNCTIONS,
+      EVENTS,
+      ENTRIES,
+      VARIABLES,
+      NUMBERS,
+      LITERALS,
+      REGEX,
+      FUNCTION_USE
+    ]
+  };
 };
